cd1716ac7bc475a8964b7beed011d2f41046e401
Kaushal Nigam <nigam.kaushal@gmail.com>
2022-04-17 16:37:23 +0530

    create igw,routetable, elasticip. allocate elastic ip create instance with public ip

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,7 @@ import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
 import com.amazonaws.services.ec2.model.*;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,87 +29,106 @@ public class App
     static final AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard().withRegion(Regions.US_EAST_1)
             .withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
 
-    public static void startInstance(String instance_id)
+    public static void startInstance(String instanceId)
     {
-        DryRunSupportedRequest<StartInstancesRequest> dry_request =
+        DryRunSupportedRequest<StartInstancesRequest> dryRequest =
                 () -> {
                     StartInstancesRequest request = new StartInstancesRequest()
-                            .withInstanceIds(instance_id);
+                            .withInstanceIds(instanceId);
 
                     return request.getDryRunRequest();
                 };
 
-        DryRunResult dry_response = ec2.dryRun(dry_request);
+        DryRunResult<StartInstancesRequest> dryResponse = ec2.dryRun(dryRequest);
 
-        if(!dry_response.isSuccessful()) {
+        if(!dryResponse.isSuccessful()) {
             System.out.printf(
-                    "Failed dry run to start instance %s", instance_id);
+                    "Failed dry run to start instance %s", instanceId);
 
-            throw dry_response.getDryRunResponse();
+            throw dryResponse.getDryRunResponse();
         }
 
         StartInstancesRequest request = new StartInstancesRequest()
-                .withInstanceIds(instance_id);
+                .withInstanceIds(instanceId);
 
         ec2.startInstances(request);
 
-        System.out.printf("Successfully started instance %s", instance_id);
+        System.out.printf("Successfully started instance %s", instanceId);
     }
 
-    public static void stopInstance(String instance_id)
+    public static void stopInstance(String instanceId)
     {
-        DryRunSupportedRequest<StopInstancesRequest> dry_request =
+        DryRunSupportedRequest<StopInstancesRequest> dryRequest =
                 () -> {
                     StopInstancesRequest request = new StopInstancesRequest()
-                            .withInstanceIds(instance_id);
+                            .withInstanceIds(instanceId);
 
                     return request.getDryRunRequest();
                 };
 
-        DryRunResult dry_response = ec2.dryRun(dry_request);
+        DryRunResult<StopInstancesRequest> dryResponse = ec2.dryRun(dryRequest);
 
-        if(!dry_response.isSuccessful()) {
+        if(!dryResponse.isSuccessful()) {
             System.out.printf(
-                    "Failed dry run to stop instance %s", instance_id);
-            throw dry_response.getDryRunResponse();
+                    "Failed dry run to stop instance %s", instanceId);
+            throw dryResponse.getDryRunResponse();
         }
 
         StopInstancesRequest request = new StopInstancesRequest()
-                .withInstanceIds(instance_id);
+                .withInstanceIds(instanceId);
 
         ec2.stopInstances(request);
 
-        System.out.printf("Successfully stop instance %s", instance_id);
+        System.out.printf("Successfully stop instance %s", instanceId);
     }
 
-    public static void createInstance(String name, String amiId, String subnetId){
-        RunInstancesRequest run_request = new RunInstancesRequest()
+    public static void createKeyPair(String keyName){
+        CreateKeyPairRequest createKeyPairRequest = new CreateKeyPairRequest()
+                .withKeyName(keyName);
+        CreateKeyPairResult createKeyPairResult = ec2.createKeyPair(createKeyPairRequest);
+        KeyPair keyPair = createKeyPairResult.getKeyPair();
+        String privateKey = keyPair.getKeyMaterial();
+        File file = new File(keyName + ".pem");
+        try (FileWriter fw = new FileWriter(file)){
+            fw.append(privateKey);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String createInstance(String name, String amiId, String subnetId, String keyName){
+        InstanceNetworkInterfaceSpecification interfaceSpecification = new InstanceNetworkInterfaceSpecification()
+                .withDeviceIndex(0)
+                .withAssociatePublicIpAddress(true)
+                .withSubnetId(subnetId);
+        RunInstancesRequest runRequest = new RunInstancesRequest()
                 .withImageId(amiId)
-                .withSubnetId(subnetId)
                 .withInstanceType(InstanceType.T2Micro)
                 .withMaxCount(1)
-                .withMinCount(1);
+                .withMinCount(1)
+                .withNetworkInterfaces(interfaceSpecification)
+                .withKeyName(keyName);
 
-        RunInstancesResult run_response = ec2.runInstances(run_request);
-
-        String reservation_id = run_response.getReservation().getInstances().get(0).getInstanceId();
+        RunInstancesResult runResponse = ec2.runInstances(runRequest);
+        String reservationId = runResponse.getReservation().getInstances().get(0).getInstanceId();
 
         Tag tag = new Tag()
                 .withKey("Name")
                 .withValue(name);
 
-        CreateTagsRequest tag_request = new CreateTagsRequest()
-                .withResources(reservation_id)
+        CreateTagsRequest tagRequest = new CreateTagsRequest()
+                .withResources(reservationId)
                 .withTags(tag);
 
-        ec2.createTags(tag_request);
+        ec2.createTags(tagRequest);
 
         System.out.printf(
-                "Successfully started EC2 instance %s based on AMI %s",
-                reservation_id, amiId);
+                "Successfully started EC2 instance %s based on AMI %s %n",
+                reservationId, amiId);
+        return  reservationId;
     }
 
-    public static void createVpc(String name){
+    public static String createVpc(String name){
         Tag tag = new Tag()
             .withKey("Name")
             .withValue(name);
@@ -122,6 +142,7 @@ public class App
                 .withTagSpecifications(tagSpecification);
         CreateVpcResult vpcResult = ec2.createVpc(vpcRequest);
         System.out.println(vpcResult.getVpc().toString());
+        return vpcResult.getVpc().getVpcId();
     }
 
     public static void setVpcTag(List<Tag> tagList, String vpcId){
@@ -136,7 +157,7 @@ public class App
         ec2.deleteTags(tagsRequest);
     }
 
-    public static void createSubnet(String name, String vpcId, String cidr, String availabilityZone){
+    public static String createSubnet(String name, String vpcId, String cidr, String availabilityZone){
         Tag tag = new Tag()
                 .withKey("Name")
                 .withValue(name);
@@ -150,13 +171,85 @@ public class App
 
         CreateSubnetResult subnetResult = ec2.createSubnet(subnetRequest);
         System.out.println(subnetResult.toString());
+        return subnetResult.getSubnet().getSubnetId();
+    }
+
+    public static String createIGW(String name, String vpcId){
+        Tag tag = new Tag()
+                .withKey("Name")
+                .withValue(name);
+        ResourceType resourceType = ResourceType.InternetGateway;
+        TagSpecification tagSpecification = new TagSpecification();
+        tagSpecification.withTags(tag);
+        tagSpecification.setResourceType(resourceType);
+        CreateInternetGatewayRequest request = new CreateInternetGatewayRequest()
+                .withTagSpecifications(tagSpecification);
+
+        CreateInternetGatewayResult result = ec2.createInternetGateway(request);
+        System.out.println(result.toString());
+        AttachInternetGatewayRequest attachInternetGatewayRequest = new AttachInternetGatewayRequest()
+                .withInternetGatewayId(result.getInternetGateway().getInternetGatewayId())
+                .withVpcId(vpcId);
+        ec2.attachInternetGateway(attachInternetGatewayRequest);
+        return result.getInternetGateway().getInternetGatewayId();
+    }
+
+    public static void createRouteTable(String name, String vpcId, String subnetId, String igwId){
+        Tag tag = new Tag()
+                .withKey("Name")
+                .withValue(name);
+        ResourceType resourceType = ResourceType.RouteTable;
+        TagSpecification tagSpecification = new TagSpecification();
+        tagSpecification.withTags(tag);
+        tagSpecification.setResourceType(resourceType);
+        CreateRouteTableRequest request = new CreateRouteTableRequest()
+                .withTagSpecifications(tagSpecification)
+                .withVpcId(vpcId);
+        CreateRouteTableResult result = ec2.createRouteTable(request);
+        System.out.println(result.toString());
+        AssociateRouteTableRequest associateRouteTableRequest = new AssociateRouteTableRequest()
+                .withRouteTableId(result.getRouteTable().getRouteTableId())
+                .withSubnetId(subnetId);
+        ec2.associateRouteTable(associateRouteTableRequest);
+        CreateRouteRequest createRouteRequest = new CreateRouteRequest()
+                .withRouteTableId(result.getRouteTable().getRouteTableId())
+                .withDestinationCidrBlock("0.0.0.0/0")
+                .withGatewayId(igwId);
+        ec2.createRoute(createRouteRequest);
+    }
+
+    public static void allocateElasticIP(String elasticIP, String instanceId){
+
+        AssociateAddressRequest associateAddressRequest =
+                new AssociateAddressRequest()
+                        .withInstanceId(instanceId)
+                        .withPublicIp(elasticIP);
+
+        AssociateAddressResult associateAddressResult =
+                ec2.associateAddress(associateAddressRequest);
+        System.out.println(associateAddressResult.toString());
+    }
+
+    public static String createElasticAddress(String name){
+        Tag tag = new Tag()
+                .withKey("Name")
+                .withValue(name);
+        ResourceType resourceType = ResourceType.ElasticIp;
+        TagSpecification tagSpecification = new TagSpecification();
+        tagSpecification.withTags(tag);
+        tagSpecification.setResourceType(resourceType);
+        AllocateAddressRequest allocateAddressRequest = new AllocateAddressRequest()
+                .withTagSpecifications(tagSpecification);
+        AllocateAddressResult allocateAddressResult = ec2.allocateAddress(allocateAddressRequest);
+        System.out.println(allocateAddressResult.toString());
+        return allocateAddressResult.getPublicIp();
     }
 
     public static void main( String[] args )
     {
         // Start/Stop Instance
 //        boolean start = false;
-//        String instanceId = "i-05820cfc84498430a";
+//        String instanceId = "instanceId";
 //        if(start) {
 //            startInstance(instanceId);
 //        } else {
@@ -164,23 +257,41 @@ public class App
 //        }
 
 
-        // Create instance
-//        createInstance("test", "amiId", "subnetId");
-
         // Create Vpc with tag
-//        createVpc("test");
+//        String vpcId = createVpc("testVPC");
 
 
         // Set VPC tags
 //        List<Tag> tagList = Arrays.asList(new Tag("Name", "changed"), new Tag("Owner", "Kaushal"), new Tag("Owner 2", "sumit"));
-//        setVpcTag(tagList, "vpc-0e748fca053aa1ffd");
+//        setVpcTag(tagList, "vpcId");
 
 
         // Delete VPC Tags
 //        List<Tag> tagList = Arrays.asList(new Tag("Owner"), new Tag("Owner 2"));
-//        deleteVpcTag(tagList, "vpc-0e748fca053aa1ffd");
+//        deleteVpcTag(tagList, "vpcId");
 
         // Create Subnet
-//        createSubnet("Test", "vpc-0e748fca053aa1ffd", "10.0.0.0/27", "us-east-1b");
+//        String subnetId = createSubnet("TestSubnet", vpcId, "10.0.0.0/27", "us-east-1b");
+
+        // Create IGW
+//        String igwId = createIGW("testIGW", vpcId);
+
+        // Create RouteTable
+//        createRouteTable("testRouteTable", vpcId, subnetId, igwId);
+
+        // Create Keypair
+//        createKeyPair("keypair");
+
+        // Create instance
+//        String instanceId = createInstance("testInstance", "ami-03ededff12e34e59e", subnetId, "keypair");
+
+        // Create Elastic IP
+//        String elasticIP = createElasticAddress("testElastic");
+
+        // Associate Elastic IP
+//        allocateElasticIP(elasticIP, instanceId);
+
+        // NAT gateway, endpoints pending
+
     }
 }
