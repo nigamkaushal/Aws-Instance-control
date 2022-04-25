08d77dd2f94f0fcdaf5a9df0859c5ad083fa2d31
Kaushal Nigam <nigam.kaushal@gmail.com>
2022-04-25 15:39:09 +0530

    delete vpc,terminate instance,delete keypair,create modify attach delete volume,create snapshot, create volume from snapshot,create targetgroup

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,13 +6,19 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
 import com.amazonaws.services.ec2.model.*;
-import com.amazonaws.util.Base64;
+import com.amazonaws.services.ec2.model.Tag;
+import com.amazonaws.services.elasticloadbalancingv2.AmazonElasticLoadBalancing;
+import com.amazonaws.services.elasticloadbalancingv2.AmazonElasticLoadBalancingClient;
+import com.amazonaws.services.elasticloadbalancingv2.AmazonElasticLoadBalancingClientBuilder;
+import com.amazonaws.services.elasticloadbalancingv2.model.*;
 
 import java.io.*;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 public class App
@@ -28,7 +34,10 @@ public class App
         }
     }
 
-    static final AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard().withRegion(Regions.AP_SOUTH_1)
+    static final AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard().withRegion(Regions.US_EAST_1)
+            .withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
+
+    static final AmazonElasticLoadBalancing elb = AmazonElasticLoadBalancingClientBuilder.standard().withRegion(Regions.US_EAST_1)
             .withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
 
     public static void startInstance(String instanceId)
@@ -98,18 +107,29 @@ public class App
         }
     }
 
+    public static void deleteKeyPair(String keyName){
+        DeleteKeyPairRequest deleteKeyPairRequest = new DeleteKeyPairRequest()
+                .withKeyName(keyName);
+        ec2.deleteKeyPair(deleteKeyPairRequest);
+        try {
+            Files.delete(Path.of(keyName + ".pem"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static String createInstance(String name, String amiId, String subnetId, String keyName){
         InstanceNetworkInterfaceSpecification interfaceSpecification = new InstanceNetworkInterfaceSpecification()
                 .withDeviceIndex(0)
                 .withAssociatePublicIpAddress(true)
                 .withSubnetId(subnetId);
-        String data = "#!/bin/bash \n" + "amazon-linux-extras install nginx \n" + "systemctl start nginx \n";
-        String base64Data = null;
-        try {
-            base64Data = new String(Base64.encode(data.getBytes(StandardCharsets.UTF_8)), "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        }
+//        String data = "#!/bin/bash \n" + "amazon-linux-extras install nginx \n" + "systemctl start nginx \n";
+//        String base64Data = null;
+//        try {
+//            base64Data = new String(Base64.encode(data.getBytes(StandardCharsets.UTF_8)), "UTF-8");
+//        } catch (UnsupportedEncodingException e) {
+//            throw new RuntimeException(e);
+//        }
 
         RunInstancesRequest runRequest = new RunInstancesRequest()
                 .withImageId(amiId)
@@ -117,8 +137,8 @@ public class App
                 .withMaxCount(1)
                 .withMinCount(1)
                 .withNetworkInterfaces(interfaceSpecification)
-                .withKeyName(keyName)
-                .withUserData(base64Data);
+                .withKeyName(keyName);
+//                .withUserData(base64Data);
 
         RunInstancesResult runResponse = ec2.runInstances(runRequest);
         String reservationId = runResponse.getReservation().getInstances().get(0).getInstanceId();
@@ -139,6 +159,12 @@ public class App
         return  reservationId;
     }
 
+    public static void terminateInstance(String instanceId){
+        TerminateInstancesRequest instancesRequest = new TerminateInstancesRequest()
+                .withInstanceIds(instanceId);
+        ec2.terminateInstances(instancesRequest);
+    }
+
     public static String createVpc(String name){
         Tag tag = new Tag()
             .withKey("Name")
@@ -154,6 +180,12 @@ public class App
         CreateVpcResult vpcResult = ec2.createVpc(vpcRequest);
         System.out.println(vpcResult.getVpc().toString());
         return vpcResult.getVpc().getVpcId();
+    }
+
+    public static void deleteVpc(String vpcId){
+        DeleteVpcRequest deleteVpcRequest = new DeleteVpcRequest()
+                .withVpcId(vpcId);
+        ec2.deleteVpc(deleteVpcRequest);
     }
 
     public static void setVpcTag(List<Tag> tagList, String vpcId){
@@ -327,6 +359,139 @@ public class App
         return securityGroupResult.getGroupId();
     }
 
+    public static String createVolume(String name, int size, String az){
+        Tag tag = new Tag()
+                .withKey("Name")
+                .withValue(name);
+        ResourceType resourceType = ResourceType.Volume;
+        TagSpecification tagSpecification = new TagSpecification();
+        tagSpecification.withTags(tag);
+        tagSpecification.setResourceType(resourceType);
+        CreateVolumeRequest createVolumeRequest = new CreateVolumeRequest()
+                .withTagSpecifications(tagSpecification)
+                .withAvailabilityZone(az)
+                .withVolumeType(VolumeType.Gp2)
+                .withSize(size)
+                .withEncrypted(false);
+        CreateVolumeResult volumeResult = ec2.createVolume(createVolumeRequest);
+        System.out.println(volumeResult.toString());
+        return volumeResult.getVolume().getVolumeId();
+    }
+
+    public static void deleteVolume(String volumeId){
+        DeleteVolumeRequest deleteVolumeRequest = new DeleteVolumeRequest()
+                .withVolumeId(volumeId);
+        ec2.deleteVolume(deleteVolumeRequest);
+    }
+
+    public static void modifyVolume(String volumeId, int size){
+        ModifyVolumeRequest modifyVolumeRequest = new ModifyVolumeRequest()
+                .withVolumeId(volumeId)
+                .withSize(size);
+        ec2.modifyVolume(modifyVolumeRequest);
+    }
+
+    public static void attachVolume(String volumeId, String instanceId, String device){
+        AttachVolumeRequest attachVolumeRequest = new AttachVolumeRequest()
+                .withVolumeId(volumeId)
+                .withInstanceId(instanceId)
+                .withDevice(device);
+        ec2.attachVolume(attachVolumeRequest);
+    }
+
+    public static void detachVolume(String volumeId){
+        DetachVolumeRequest detachVolumeRequest = new DetachVolumeRequest()
+                .withVolumeId(volumeId)
+                .withForce(true);
+        ec2.detachVolume(detachVolumeRequest);
+    }
+
+    public static String createSnapshot(String name, String volumeId){
+        Tag tag = new Tag()
+                .withKey("Name")
+                .withValue(name);
+        ResourceType resourceType = ResourceType.Snapshot;
+        TagSpecification tagSpecification = new TagSpecification();
+        tagSpecification.withTags(tag);
+        tagSpecification.setResourceType(resourceType);
+        CreateSnapshotRequest createSnapshotRequest = new CreateSnapshotRequest()
+                .withTagSpecifications(tagSpecification)
+                .withVolumeId(volumeId)
+                .withDescription("SnapShot of volume");
+        CreateSnapshotResult createSnapshotResult = ec2.createSnapshot(createSnapshotRequest);
+        System.out.println(createSnapshotResult.toString());
+        DescribeSnapshotsRequest describeSnapshotsRequest = new DescribeSnapshotsRequest()
+                .withSnapshotIds(createSnapshotResult.getSnapshot().getSnapshotId());
+        Snapshot snapshot = null;
+        do{
+            snapshot = ec2.describeSnapshots(describeSnapshotsRequest).getSnapshots().get(0);
+            if(snapshot.getState().equals("error")){
+                System.out.println("Error creating snapshot.");
+                break;
+            }
+            if(snapshot.getState().equals("completed")){
+                System.out.println("Completed creating snapshot.");
+            }
+        }while (!snapshot.getState().equals("completed"));
+        return createSnapshotResult.getSnapshot().getSnapshotId();
+    }
+
+    public static String createVolumeFromSnapshot(String name, String snapshotId, String az){
+        Tag tag = new Tag()
+                .withKey("Name")
+                .withValue(name);
+        ResourceType resourceType = ResourceType.Volume;
+        TagSpecification tagSpecification = new TagSpecification();
+        tagSpecification.withTags(tag);
+        tagSpecification.setResourceType(resourceType);
+        CreateVolumeRequest createVolumeRequest = new CreateVolumeRequest()
+                .withTagSpecifications(tagSpecification)
+                .withSnapshotId(snapshotId)
+                .withAvailabilityZone(az);
+        CreateVolumeResult volumeResult = ec2.createVolume(createVolumeRequest);
+        System.out.println(volumeResult);
+        return volumeResult.getVolume().getVolumeId();
+    }
+
+    public static String createTargetGroup(String name, String vpcId, String... instanceIds){
+        CreateTargetGroupRequest createTargetGroupRequest = new CreateTargetGroupRequest()
+                .withTargetType(TargetTypeEnum.Instance)
+                .withName(name)
+                .withTags(new com.amazonaws.services.elasticloadbalancingv2.model.Tag().withKey("Name").withValue(name))
+                .withProtocol(ProtocolEnum.TCP)
+                .withPort(80)
+                .withVpcId(vpcId)
+                .withHealthCheckProtocol(ProtocolEnum.TCP);
+        CreateTargetGroupResult createTargetGroupResult = elb.createTargetGroup(createTargetGroupRequest);
+        List<TargetDescription> targetDescriptions = new ArrayList<>();
+        for(String instanceId : instanceIds)
+            targetDescriptions.add(new TargetDescription().withId(instanceId));
+        RegisterTargetsRequest registerTargetsRequest = new RegisterTargetsRequest()
+                .withTargets(targetDescriptions)
+                .withTargetGroupArn(createTargetGroupResult.getTargetGroups().get(0).getTargetGroupArn());
+        elb.registerTargets(registerTargetsRequest);
+        System.out.println(createTargetGroupResult);
+        return createTargetGroupResult.getTargetGroups().toString();
+    }
+
+    public static void getSubnetsFromVpc(String vpcId){
+        DescribeSubnetsRequest describeSubnetsRequest = new DescribeSubnetsRequest()
+                .withFilters(new Filter().withName("vpc-id").withValues(vpcId));
+        DescribeSubnetsResult describeSubnetsResult = ec2.describeSubnets(describeSubnetsRequest);
+        System.out.println(describeSubnetsResult.getSubnets());
+    }
+
+    // Pending
+    public static void createLoadBalancer(String name, LoadBalancerTypeEnum type, LoadBalancerSchemeEnum scheme){
+
+        CreateLoadBalancerRequest createLoadBalancerRequest = new CreateLoadBalancerRequest()
+                .withTags(new com.amazonaws.services.elasticloadbalancingv2.model.Tag().withKey("Name").withValue(name))
+                .withName(name)
+                .withType(type)
+                .withScheme(scheme)
+                .withSubnetMappings();
+    }
+
     public static void main( String[] args )
     {
         // Start/Stop Instance
@@ -338,8 +503,10 @@ public class App
 //            stopInstance(instanceId);
 //        }
 
+//        deleteKeyPair("keypair");
+
         // Create Keypair
-//        createKeyPair("keypair2");
+//        createKeyPair("keypair");
 
         // Create Elastic IP
 //        String elasticIP = createElasticAddress("testElastic");
@@ -354,13 +521,12 @@ public class App
 //        List<Tag> tagList = Arrays.asList(new Tag("Name", "changed"), new Tag("Owner", "Kaushal"), new Tag("Owner 2", "sumit"));
 //        setVpcTag(tagList, "vpcId");
 
-
         // Delete VPC Tags
 //        List<Tag> tagList = Arrays.asList(new Tag("Owner"), new Tag("Owner 2"));
 //        deleteVpcTag(tagList, "vpcId");
 
         // Create Subnet
-//        String subnetId = createSubnet("TestSubnet", vpcId, "10.0.0.0/27", "ap-south-1a");
+//        String subnetId = createSubnet("TestSubnet", vpcId, "10.0.0.0/27", "us-east-1a");
 
         // Create IGW
 //        String igwId = createIGW("testIGW", vpcId);
@@ -372,7 +538,7 @@ public class App
 //        createRouteTableWithIGW("testRouteTable", vpcId, subnetId, natGId);
 
         // Create instance
-//        String instanceId = createInstance("testInstance", "ami-0a3277ffce9146b74", subnetId, "keypair2");
+//        String instanceId = createInstance("testInstance", "ami-0f9fc25dd2506cf6d", subnetId, "keypair");
 
 //        String allocationId = createElasticAddress("testElastic");
 
@@ -384,10 +550,40 @@ public class App
 //            TimeUnit.SECONDS.sleep(190);
 //        } catch (InterruptedException e) {
 //            e.printStackTrace();
-
 //        }
 
         // endpoints pending
+
+        // VOLUME
+//        String volumeId = createVolume("testVolume", 50, "us-east-1a");
+//        deleteVolume("vol-01d8e8933647c9cd8");
+//        modifyVolume("vol-01d8e8933647c9cd8", 10);
+//        try {
+//            System.out.println("Waiting for 10 seconds...");
+//            TimeUnit.SECONDS.sleep(10);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        attachVolume(volumeId, instanceId, "/dev/sdf");
+//        detachVolume(volumeId);
+
+//        String snapshotId = createSnapshot("testSnapshot", volumeId);
+
+//        try {
+//            System.out.println("Waiting for 30 seconds...");
+//            TimeUnit.SECONDS.sleep(30);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+//        createVolumeFromSnapshot("VolumeSnapshot", "snap-052c9e7192e6d9f49","us-east-1a");
+
+
+        // Create Target Group
+//        createTargetGroup("testTargetGroup", "vpc-095d922694dfe4936", "i-0adbd2db81ca26eca");
+
+        // Create Load Balancer
+//        createLoadBalancer("TestLB", "arn:aws:elasticloadbalancing:us-east-1:141259127249:targetgroup/testTargetGroup/597755878d45d261")
 
     }
 }
